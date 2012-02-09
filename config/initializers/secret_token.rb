d90d9cfbc44d6ccada5898737800be6f78ed05e7
sunchess <sunchess@inbox.ru>
2012-02-09 14:47:27 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gdedk::Application.config.secret_token = '219f62d02adf904b41e4aa2e99dd72522587d5ac46e6d0fb19d206851d48e05e7007c19de7926bfe67e281106cbd8ee6218b6c7efa20c3b0f95aee6fa1765a05'
